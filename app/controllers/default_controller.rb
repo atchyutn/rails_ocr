d6d30667f6f999185addfb8983b44fe59c3d2ad5
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2018-06-11 10:24:10 +0530

    added a module for OCR code

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -1,6 +1,12 @@
 class DefaultController < ApplicationController
+  include OcrSpace
+
 
   def index
-
+    # puts hello
+    OcrSpace
   end
+  # puts "\n\n\n #{OcrSpace.hello} \n\n\n"
+
+  # puts OcrSpace::hello
 end
---
+++ lib/ocr_space.rb
@@ -0,0 +1,9 @@
+module OcrSpace
+  def self.hello
+    puts "\n\n\nHello\n\n\n"
+  end
+end
+
+
+
+OcrSpace.hello
